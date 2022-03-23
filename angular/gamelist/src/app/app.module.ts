7f0dbacdf9daff653ed9cbb42109284d78632cda
AntonioGiannini2 <agiannini@studenti.apuliadigitalmaker.it>
2022-03-23 09:43:28 +0100

    Angular stuff

---
+++ angular/gamelist/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular/gamelist/src/app/components/home/home.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { APIResponse, Game } from 'src/app/models';
+import { HttpService } from 'src/app/services/http.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit, OnDestroy {
+  public sort: string = '';
+  public games: Array<Game> = [];
+  private routeSub: Subscription = new Subscription();
+  private gameSub: Subscription = new Subscription();
+
+  constructor(
+    private httpService: HttpService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.routeSub = this.activatedRoute.params.subscribe((params: Params) => {
+      if (params['game-search']) {
+        this.searchGames('metacrit', params['game-search'])
+      } else {
+        this.searchGames('metacrit')
+      }
+    });
+  }
+
+  searchGames(sort: string, search?: string): void {
+    this.gameSub = this.httpService
+    .getGameList(sort, search)
+    .subscribe((gameList: APIResponse<Game>) => {
+      this.games = gameList.results
+      console.log(gameList)
+    });
+  }
+
+  ngOnDestroy(): void {
+
+  }
+}
